60c43f3005ae76dbd87f06decb40ffe6a9c1cd12
pacanosiu <60273697+DamianPacanowski@users.noreply.github.com>
2024-03-20 18:10:14 +0100

    Create payu.php

---
+++ payu.php
@@ -0,0 +1,25 @@
+<?php
+$ch = curl_init();
+$clientId = '300746';
+$secret = '2ee86a66e5d97e3fadc400c9f19b065d';
+curl_setopt($ch, CURLOPT_URL, 'https://secure.payu.com/pl/standard/user/oauth/authorize');
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERPWD, $clientId . ':' . $secret);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials');
+$result = curl_exec($ch);
+if(empty($result)) 
+{
+    die('Error: No response.');
+} 
+else 
+{
+    $json=json_decode($result);
+    echo 'Access Token: '.$json->access_token;
+	  var_dump($json);
+	  var_dump($json->access_token);
+}
+curl_close($ch);
+?>
